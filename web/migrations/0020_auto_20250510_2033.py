aa37a9941effee7d5c47d5f099682ce56bb1bbc5
agutcan <aaronolo121@gmail.com>
2025-06-07 20:51:44 +0200

    arreglo poblate

--- web/migrations/0020_auto_20250510_2033.py
+++ web/migrations/0020_auto_20250510_2033.py
@@ -212,7 +212,7 @@ def poblar_datos(apps, schema_editor):
         Tournament(name="8 teams 8 teams inactive", game=games[1],  max_player_per_team=1, max_teams=8, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
         Tournament(name="4 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
 
-        Tournament(name="4 teams matchs", game=games[0],  max_player_per_team=2, max_teams=4, start_date = timezone.now() + timedelta(minutes=5), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="4 teams matchs", game=games[0],  max_player_per_team=2, max_teams=2, start_date = timezone.now() + timedelta(minutes=5), prize_pool=1000.00, created_by=users[0]),
         Tournament(name="4 teams multi 1", game=games[0],  max_player_per_team=1, max_teams=2, start_date = timezone.now() + timedelta(days=1), prize_pool=1000.00, created_by=users[0]),
         Tournament(name="4 teams multi 2", game=games[0],  max_player_per_team=1, max_teams=2, start_date = timezone.now() + timedelta(minutes=6), prize_pool=1000.00, created_by=users[0]),
     ]
@@ -243,10 +243,10 @@ def poblar_datos(apps, schema_editor):
         TournamentTeam(tournament=tournaments[4], team=teams[20]),
         TournamentTeam(tournament=tournaments[4], team=teams[21]),
 
-        TournamentTeam(tournament=tournaments[5], team=teams[21]),
         TournamentTeam(tournament=tournaments[5], team=teams[22]),
-        TournamentTeam(tournament=tournaments[6], team=teams[23]),
+        TournamentTeam(tournament=tournaments[5], team=teams[23]),
         TournamentTeam(tournament=tournaments[6], team=teams[24]),
+        TournamentTeam(tournament=tournaments[6], team=teams[25]),
     ]
     TournamentTeam.objects.bulk_create(tournament_teams)  # Guardar todos los equipos en los torneos
 
