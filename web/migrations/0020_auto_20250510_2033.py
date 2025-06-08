5f3aa31eff5e8a1503548f3e7372f1ebe1ebaadb
agutcan <aaronolo121@gmail.com>
2025-06-08 20:54:41 +0200

    arreglo datos para el poblate

--- web/migrations/0020_auto_20250510_2033.py
+++ web/migrations/0020_auto_20250510_2033.py
@@ -212,13 +212,13 @@ def poblar_datos(apps, schema_editor):
 
     # Crear torneos
     tournaments = [
-        Tournament(name="4 teams 2 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
-        Tournament(name="4 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
-        Tournament(name="8 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=8, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
-        Tournament(name="8 teams 8 teams inactive", game=games[1],  max_player_per_team=1, max_teams=8, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
-        Tournament(name="4 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=2), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="4 teams 2 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=1), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="4 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=1), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="8 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=8, start_date = timezone.now() + timedelta(minutes=1), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="8 teams 8 teams inactive", game=games[1],  max_player_per_team=1, max_teams=8, start_date = timezone.now() + timedelta(minutes=1), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="4 teams 4 teams inactive", game=games[1],  max_player_per_team=1, max_teams=4, start_date = timezone.now() + timedelta(minutes=1), prize_pool=1000.00, created_by=users[0]),
 
-        Tournament(name="2 teams matchs", game=games[0],  max_player_per_team=2, max_teams=2, start_date = timezone.now() + timedelta(minutes=5), prize_pool=1000.00, created_by=users[0]),
+        Tournament(name="2 teams matchs", game=games[0],  max_player_per_team=2, max_teams=2, start_date = timezone.now() + timedelta(minutes=4), prize_pool=1000.00, created_by=users[0]),
         Tournament(name="2 teams multi 1", game=games[0],  max_player_per_team=1, max_teams=2, start_date = timezone.now() + timedelta(days=1), prize_pool=1000.00, created_by=users[0]),
         Tournament(name="2 teams multi 2", game=games[0],  max_player_per_team=1, max_teams=2, start_date = timezone.now() + timedelta(minutes=6), prize_pool=1000.00, created_by=users[0]),
     ]
